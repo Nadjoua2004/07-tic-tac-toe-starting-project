3653722ab6194cbb7ef805bdda4b0fe777a07700
Nadjoua2004 <nadjoua.sahnoune@gmail.com>
2025-02-27 00:48:45 +0100

    first commit

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,13 @@
+export default function GameOver({winner, onRestart}){
+    return(
+        <div id="game-over">
+            <h2>Game Over</h2>
+           { winner && <p>{winner}  win !</p>}
+           { !winner && <p> No one win !</p>}
+            <p>
+                <button onClick={onRestart}>Restart!</button>
+            </p>
+        </div>
+    );
+
+}
